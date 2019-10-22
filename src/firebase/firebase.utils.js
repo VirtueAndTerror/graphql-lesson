2a3c3f09a15c0e5e7e2bb868051839b7b50a8b20
VirtueAndTerror <50920185+VirtueAndTerror@users.noreply.github.com>
2019-10-22 17:42:22 +0200

    added apollo-boost, react-apollo and graphql, configured graphql connections, created CollectionsOverviewContainer

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: 'AIzaSyBgOslrj6gg6djD4c5nTbk400Fo5faxcT4',
+  authDomain: 'crwn-db-48047.firebaseapp.com',
+  databaseURL: 'https://crwn-db-48047.firebaseio.com',
+  projectId: 'crwn-db-48047',
+  storageBucket: '',
+  messagingSenderId: '120995028956',
+  appId: '1:120995028956:web:57b404d4044a67a1'
 };
 
 firebase.initializeApp(config);
